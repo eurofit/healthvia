88c541cf0cce9fa188280bcc89ceaac919b5ae17
abdirazak <abdurazak.awil1@gmail.com>
2025-05-28 00:20:33 +0300

    add lottie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
+import constructionAnimation from '@/assets/lottie/ui-construction.json';
+import { Lottie } from '@/components/lottie';
 import { ReloadButton } from '@/components/reload-button';
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <Image
-        src="https://www.svgrepo.com/show/426192/cogs-settings.svg"
-        alt="Logo"
-        className="mb-8 h-40"
-        width={160}
-        height={160}
+      <Lottie
+        animationData={constructionAnimation}
+        loop
+        className="m-0 -mt-20 -mb-14 size-80"
+        aria-label="Site under construction animation"
+        style={{ maxWidth: '100%' }}
       />
-      <h1 className="mb-2 text-center text-3xl font-bold">Site is under construction</h1>
-      <p className="text-muted-foreground mb-8 max-w-sm text-center text-lg text-pretty">
-        We&apos;re working hard to bring you the best user experience. Stay tuned!
-      </p>
+      <hgroup>
+        <h1 className="mb-2 text-center text-3xl font-bold">
+          Site is under construction
+        </h1>
+        <p className="text-muted-foreground mb-8 max-w-sm text-center text-lg text-pretty">
+          We&apos;re working hard to bring you the best user experience. Stay tuned!
+        </p>
+      </hgroup>
       <div className="flex space-x-4">
         <Button className="font-medium" size="lg" asChild>
           <Link href="/contact-us">Contact Us</Link>
---
+++ src/components/lottie.tsx
@@ -0,0 +1,8 @@
+'use client';
+import LottieComp from 'lottie-react';
+
+import { LottieComponentProps } from 'lottie-react';
+
+export function Lottie(props: LottieComponentProps) {
+  return <LottieComp {...props} />;
+}
